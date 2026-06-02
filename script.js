1559a5adcadfecd53a11ff9d9d4823a2b5410032
MuskanParmar-mp <parmarmuskan1362006@gmail.com>
2026-06-02 14:13:26 +0530

    code

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-document.getElementsByTagName("h1")
+let heading = document.getElementById("heading");
+console.log(heading);
+
+let text = document.getElementsByClassName("text");
+console.log(text);j
